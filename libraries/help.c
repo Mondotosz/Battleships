e47083d70b6dc2baed1aa1f8241deb733da164cb
Mon <55015209+Mondotosz@users.noreply.github.com>
2020-03-04 20:45:20 +0100

    implemented displayHelp function

---
+++ libraries/help.c
@@ -0,0 +1,33 @@
+//
+// Created by mondotosz on 04/03/2020.
+//
+
+#include "help.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Displays the help info
+ */
+void displayHelp() {
+    //displays help
+    system("cls");
+    printf("Help\n");
+    printf("\n");
+    printf("The goal of this game is to sink every boat in the grid.\n");
+    printf("To do so you'll need to input coordinates to hit them.\n");
+    printf("\n");
+    printf("Each coordinate will be marked as such :\n");
+    printf("\n");
+    printf("\t%c%c=unchecked\n",176,176);
+    printf("\t  =miss\n");
+    printf("\t%c%c=hit\n",219,219);
+    printf("\n");
+    printf("Good luck on the sea young sailor!\n");
+
+    //wait for input to return
+    printf("\n");
+    printf("press enter to return");
+    fflush(stdin);
+    getchar();
+}
