8efe087148a1e4dc2106a96b0c306423cb44dba9
Mon <55015209+Mondotosz@users.noreply.github.com>
2020-03-06 15:39:31 +0100

    Declared gameHub along with its dependencies

--- libraries/game.h
+++ libraries/game.h
@@ -5,4 +5,17 @@
 #ifndef BATTLESHIPS_GAME_H
 #define BATTLESHIPS_GAME_H
 
+#define bool	_Bool
+#define true	1
+#define false	0
+
+#define MAX_NICKNAME_LENGTH 16
+
+typedef struct{
+    char nickname[MAX_NICKNAME_LENGTH];
+    bool authenticated;
+}users;
+
+void gameHub(users currentUser);
+
 #endif //BATTLESHIPS_GAME_H
