4aecdd831c68c13900e04350e05230452c0b2100
Mon <55015209+Mondotosz@users.noreply.github.com>
2020-03-06 15:39:06 +0100

    Created the test version of gameHub

--- libraries/game.c
+++ libraries/game.c
@@ -3,3 +3,37 @@
 //
 
 #include "game.h"
+#include "authentication.h"
+
+/**
+ * main game function
+ */
+void gameHub(users currentUser){
+    bool win=false;
+    int stateGrid[9][9];
+    //hard codded grid
+    int checkGrid[9][9]={
+            {0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0},
+            {0,0,0,1,0,0,0,0,0},
+            {0,0,0,1,0,0,0,0,0},
+            {0,0,0,1,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0}
+    };
+
+    //setup
+    for (int i = 0; i < sizeof(stateGrid)/ sizeof(stateGrid[0]); ++i) {
+        for (int j = 0; j < sizeof(stateGrid)/ sizeof(stateGrid[0]); ++j) {
+            stateGrid[i][j]=0;
+        }
+    }
+
+    //game
+    do{
+
+    }while (win==false);
+
+}
