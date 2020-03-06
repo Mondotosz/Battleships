a58a308a09df0c7cc6976f6f3ec6181568c4c71d
Mon <55015209+Mondotosz@users.noreply.github.com>
2020-03-06 14:24:40 +0100

    Created trueFalse function

--- libraries/utilities/utilities.c
+++ libraries/utilities/utilities.c
@@ -4,11 +4,36 @@
 
 #include "utilities.h"
 #include <stdio.h>
+#include <string.h>
 
-void pause(){
+void pause() {
     //wait for input to return
     printf("\n");
     printf("press enter to return");
     fflush(stdin);
     getchar();
+}
+
+bool trueFalse() {
+    char answer;
+    bool result;
+
+    //displays the two available answers
+    printf("(y/n)\n");
+    printf(": ");
+
+    //asks until the answer is valid
+    do {
+        fflush(stdin);
+        scanf("%s", &answer);
+    } while (strcmp(&answer, "y") != 0 && strcmp(&answer, "n") != 0);
+
+    //compare the answer in order to return true or false
+    if (strcmp(&answer, "y") == 0) {
+        result = true;
+    } else {
+        result = false;
+    }
+
+    return result;
 }
