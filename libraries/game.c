63be87209644d77c7616d2c99b46f75c2777176c
Mon <55015209+Mondotosz@users.noreply.github.com>
2020-03-06 16:29:21 +0100

    Created fire(grids stateGrid)

--- libraries/game.c
+++ libraries/game.c
@@ -37,6 +37,32 @@ void gameHub(users currentUser) {
 
 }
 
+grids fire(grids stateGrid){
+    int x;
+    char y;
+    int convertedY;
+
+    printf("\n");
+    //asks for x coordinates
+    printf("x : ");
+    do{
+        scanf("%d",&x);
+    }while (x<0||x>MAX_X);
+
+    //TODO:create char to int as a separate function
+    //asks for y coordinates and convert them
+    printf("y : ");//char
+    do{
+        scanf("%c",&y);
+        convertedY= (int)(y)-65;
+    }while (convertedY<0||convertedY>MAX_Y);
+
+    //puts them in stateGrid
+    stateGrid.grid[x][convertedY]=CHECKING;
+
+    return stateGrid;
+}
+
 bool checkWin(grids stateGrid, grids checkGrid) {
     bool win = true;
 
