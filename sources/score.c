23b521e36706f84ca5bbf9e1f2a777482d10cd4d
Mon <55015209+Mondotosz@users.noreply.github.com>
2020-03-12 18:47:41 +0100

    scoreboard now tells the user there if there are no scores

--- sources/score.c
+++ sources/score.c
@@ -8,8 +8,9 @@
 #include "utilities/utilities.h"
 #include "score.h"
 
-//TODO:Write scores to files
-
+/**
+ * displays scores from score file
+ */
 void displayScores() {
     scoreboard currentScoreboard;
 
@@ -20,94 +21,99 @@ void displayScores() {
 
     currentScoreboard = getScoreboard();
 
-    for (int i = 0; i < currentScoreboard.range; ++i) {
-        printf("nickname : %s \ttries : %d\n", currentScoreboard.existingScores[i].nickname,
-               currentScoreboard.existingScores[i].tries);
+    if (currentScoreboard.range > 0) {
+        for (int i = 0; i < currentScoreboard.range; ++i) {
+            printf("nickname : %s \ttries : %d\n", currentScoreboard.existingScores[i].nickname,
+                   currentScoreboard.existingScores[i].tries);
+        }
+    } else {
+        printf("No scores yet!\n");
     }
-
     pause();
 
 }
 
+/**
+ * gets the list of scores from a score file
+ * @return listed scores
+ */
 scoreboard getScoreboard() {
     scoreboard currentScoreboard;
     FILE *filePointer;
     char cursor;
-    char tempString[50];
+    char tempString[MAX_NICKNAME_LENGTH];
     char currentVar;
     currentScoreboard.range = 0;
 
     //opens file
     filePointer = fopen(SCOREBOARD_JSON, "r");
     if (filePointer == NULL) {
-        printf("%s doesn't exist\n", SCOREBOARD_JSON);
-        pause();
-    }
+        filePointer = fopen(SCOREBOARD_JSON, "w");
+    } else {
 
-    //gets the values in it
-    strcpy(tempString, "");
-    while (!feof(filePointer)) {
-        fflush(stdin);
-        fscanf(filePointer, "%c", &cursor);
-        //detects variable name
-        if (cursor == '"') {
-            //empty tempString
-            strcpy(tempString, "");
-            //get current var
-            do {
-                fflush(stdin);
-                fscanf(filePointer, "%c", &cursor);
-                if (cursor != '"') {
-                    strncat(tempString, &cursor, 1);
-                }
-            } while (cursor != '"');
-            //on exit we get the var name
-        }
+        //gets the values in it
+        strcpy(tempString, "");
+        while (!feof(filePointer)) {
+            fflush(stdin);
+            fscanf(filePointer, "%c", &cursor);
+            //detects variable name
+            if (cursor == '"') {
+                //empty tempString
+                strcpy(tempString, "");
+                //get current var
+                do {
+                    fflush(stdin);
+                    fscanf(filePointer, "%c", &cursor);
+                    if (cursor != '"') {
+                        strncat(tempString, &cursor, 1);
+                    }
+                } while (cursor != '"');
+                //on exit we get the var name
+            }
 
-        if (strcmp(tempString, "nickname") == 0) {
-            currentVar = NICKNAME;
-        } else if (strcmp(tempString, "tries") == 0) {
-            currentVar = TRIES;
-        } else {
-            currentVar = ' ';
-        }
+            if (strcmp(tempString, "nickname") == 0) {
+                currentVar = NICKNAME;
+            } else if (strcmp(tempString, "tries") == 0) {
+                currentVar = TRIES;
+            } else {
+                currentVar = ' ';
+            }
 
-        if (currentVar != ' ') {
-            do {
-                fflush(stdin);
-                fscanf(filePointer, "%c", &cursor);
-                if (cursor == '"') {
-                    //empty tempString
-                    strcpy(tempString, "");
-                    //get value
-                    do {
-                        fflush(stdin);
-                        fscanf(filePointer, "%c", &cursor);
-                        if (cursor != '"') {
-                            strncat(tempString, &cursor, 1);
-                        } else {
-                            switch (currentVar) {
-                                case NICKNAME:
-                                    strcpy(currentScoreboard.existingScores[currentScoreboard.range].nickname,
-                                           tempString);
-                                    break;
-                                case TRIES:
-                                    currentScoreboard.existingScores[currentScoreboard.range].tries = atoi(tempString);
-                                    currentScoreboard.range++;
-                                    break;
-                                default:
-                                    break;
+            if (currentVar != ' ') {
+                do {
+                    fflush(stdin);
+                    fscanf(filePointer, "%c", &cursor);
+                    if (cursor == '"') {
+                        //empty tempString
+                        strcpy(tempString, "");
+                        //get value
+                        do {
+                            fflush(stdin);
+                            fscanf(filePointer, "%c", &cursor);
+                            if (cursor != '"') {
+                                strncat(tempString, &cursor, 1);
+                            } else {
+                                switch (currentVar) {
+                                    case NICKNAME:
+                                        strncpy(currentScoreboard.existingScores[currentScoreboard.range].nickname,
+                                                tempString, MAX_NICKNAME_LENGTH);
+                                        break;
+                                    case TRIES:
+                                        currentScoreboard.existingScores[currentScoreboard.range].tries = atoi(
+                                                tempString);
+                                        currentScoreboard.range++;
+                                        break;
+                                }
+
+                                strcpy(tempString, "");
                             }
+                        } while (cursor != '"');
+                    }
+                } while (cursor != '"');
+            }
 
-                            strcpy(tempString, "");
-                        }
-                    } while (cursor != '"');
-                }
-            } while (cursor != '"');
         }
-
     }
-
     fclose(filePointer);
 
     return currentScoreboard;
