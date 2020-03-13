09501dc4fc4cba5fbf83eefa411b7b9736c4d81b
Mon <55015209+Mondotosz@users.noreply.github.com>
2020-03-13 21:47:34 +0100

    created boats from file function which kinda works

--- sources/game.c
+++ sources/game.c
@@ -21,20 +21,6 @@ void gameHub(users currentUser) {
     grids stateGrid;
     grids checkGrid;
     armada squadron;
-    /*
-    grids checkGrid = {{
-                               {1, 1, 1, 1, 1, 1, 1, 1, 1},
-                               {1, 1, 1, 1, 1, 1, 1, 1, 1},
-                               {1, 1, 2, 1, 1, 1, 1, 1, 1},
-                               {1, 1, 2, 1, 1, 1, 1, 1, 1},
-                               {1, 1, 2, 1, 1, 1, 1, 1, 1},
-                               {1, 1, 1, 1, 1, 1, 1, 1, 1},
-                               {1, 1, 1, 1, 1, 1, 1, 1, 1},
-                               {1, 1, 1, 1, 1, 1, 1, 1, 1},
-                               {1, 1, 1, 1, 1, 1, 1, 1, 1}}};
-*/
-
-
 
     //setup
     for (int i = 0; i < MAX_X; ++i) {
@@ -256,6 +242,11 @@ scores missCount(grids currentGrid) {
 }
 
 armada getArmada() {
+    FILE *filePointer;
+    char cursor;
+    char tempString[MAX_NICKNAME_LENGTH];
+    char currentVar;
+    int currentBoat = 0;
     armada returnedArmada = {
             {
                     {"Destroyer", 2, 0, 0, HORIZONTAL, false},
@@ -264,8 +255,101 @@ armada getArmada() {
                     {"Battleship", 4, 0, 0, HORIZONTAL, false},
                     {"Carrier", 5, 0, 0, HORIZONTAL, false}
             },
-            5
+            MAX_BOATS
     };
+
+
+    //opens file
+    filePointer = fopen(BOATS_JSON, "r");
+    if (filePointer == NULL) {
+        filePointer = fopen(BOATS_JSON, "w");
+    } else {
+
+        //gets the values in it
+        strcpy(tempString, "");
+        while (!feof(filePointer)) {
+            do {
+                fflush(stdin);
+                fscanf(filePointer, "%c", &cursor);
+            } while (cursor == ' ');
+            //detects variable name
+            if (cursor == '"') {
+                //empty tempString
+                strcpy(tempString, "");
+                //get current variable
+                do {
+                    fflush(stdin);
+                    fscanf(filePointer, "%c", &cursor);
+                    if (cursor != '"') {
+                        strncat(tempString, &cursor, 1);
+                    }
+                } while (cursor != '"');
+                //on exit we get the var name
+            }
+
+            //translate chosen variable to a char usable in switch
+            if (strcmp(tempString, "length") == 0) {
+                currentVar = BOAT_LENGTH;
+            } else if (strcmp(tempString, "direction") == 0) {
+                currentVar = BOAT_DIRECTION;
+            } else if (strcmp(tempString, "x") == 0) {
+                currentVar = BOAT_X;
+            } else if (strcmp(tempString, "y") == 0) {
+                currentVar = BOAT_Y;
+            } else if (strcmp(tempString, "exists") == 0) {
+                currentVar = BOAT_EXISTS;
+            } else {
+                currentVar = ' ';
+            }
+
+            //if the variable is known then get its value
+            if (currentVar != ' ') {
+                do {
+                    fflush(stdin);
+                    fscanf(filePointer, "%c", &cursor);
+                    //gets the value for the current variable
+                    if (cursor == '"') {
+                        strcpy(tempString, "");
+                        do {
+                            fflush(stdin);
+                            fscanf(filePointer, "%c", &cursor);
+                            if (cursor != '"') {
+                                strncat(tempString, &cursor, 1);
+                            } else {
+
+                                //TODO:replace atoi to avoid problems
+                                //puts the value in the correct variable
+                                switch (currentVar) {
+                                    case BOAT_LENGTH:
+                                        returnedArmada.navy[currentBoat].length = atoi(tempString);
+                                        break;
+                                    case BOAT_DIRECTION:
+                                        returnedArmada.navy[currentBoat].direction = tempString[0];
+                                        break;
+                                    case BOAT_X:
+                                        returnedArmada.navy[currentBoat].x = atoi(tempString) - 1;
+                                        break;
+                                    case BOAT_Y:
+                                        returnedArmada.navy[currentBoat].y = atoi(tempString) - 1;
+                                        break;
+                                    case BOAT_EXISTS:
+                                        if (strcmp(tempString, "true") == 0) {
+                                            returnedArmada.navy[currentBoat].exists = true;
+                                        }
+                                        currentBoat++;
+                                        break;
+                                }
+
+                                strcpy(tempString, "");
+                            }
+                        } while (cursor != '"');
+                    }
+                } while (cursor != '"');
+            }
+
+        }
+    }
+    fclose(filePointer);
 
     return returnedArmada;
 }
