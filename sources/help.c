683c3d85026b96fe31451fcc842e709652144d6d
Mon <55015209+Mondotosz@users.noreply.github.com>
2020-03-28 13:54:42 +0100

    updated help function to correspond to game visual

--- sources/help.c
+++ sources/help.c
@@ -12,21 +12,22 @@
  * Displays the help info
  */
 void displayHelp() {
+    const int offset = 4;
     //displays help
     system("cls");
-    printf("%sHelp%s\n",T_BOLD,T_RESET);
+    printf("%sHelp%s\n", T_BOLD, T_RESET);
     printf("\n");
     printf("The goal of this game is to sink every boat in the grid.\n");
     printf("To do so you'll need to input coordinates to hit them.\n");
     printf("\n");
     printf("Each coordinate will be marked as such :\n");
     printf("\n");
-    printf("\t%c%c=unchecked\n",176,176);
-    printf("\t  =miss\n");
-    printf("\t%c%c=hit\n",219,219);
+    printf("%*s  =unchecked\n", offset, "");
+    printf("%*s%s%c%c%s=miss\n", offset, "", T_CYAN, 176, 176, T_RESET);
+    printf("%*s%s%c%c%s=hit\n", offset, "", T_RED, 219, 219, T_RESET);
     printf("\n");
     printf("Good luck on the sea young sailor!\n");
 
     pause();
-    runtimeLog(INFO, "exited help() successfully");
+    runtimeLog(INFO, "exited help()");
 }
